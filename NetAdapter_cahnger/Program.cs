71631f38a2dc255d968e547158f4d0c9b4ab5d90
daenur <daenur@outlook.com>
2019-11-23 21:01:10 +0200

    2019-11-23

---
+++ NetAdapter_cahnger/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Management;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NetAdapter_cahnger
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                string[] netAdapters = File.ReadAllLines("netAdapters.txt");
+                string message = "";
+                try
+                {
+                    message = File.ReadAllText("message.txt");
+                }
+                catch { }
+                NetAdatersStateChange(netAdapters);
+                try
+                {
+                    message = File.ReadAllText("message.txt");
+                    MessageBox.Show(message);
+                }
+                catch { }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "[error]");
+            }
+
+        }
+
+        static void NetAdatersStateChange(params string[] list)
+        {
+            SelectQuery wmiQuery = new SelectQuery("SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionId != NULL");
+            ManagementObjectSearcher searchProcedure = new ManagementObjectSearcher(wmiQuery);
+            foreach (ManagementObject item in searchProcedure.Get())
+            {
+                //if (((string)item["NetConnectionId"]) == "name") //"Local Network Connection")
+                if (Array.IndexOf(list, (string)item["NetConnectionId"])!=-1)
+                {
+                    if (Convert.ToBoolean(item["NetEnabled"]))
+                    {
+                        item.InvokeMethod("Disable", null);
+                    }
+                    else
+                    {
+                        item.InvokeMethod("Enable", null);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
